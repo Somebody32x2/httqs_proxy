44acd6f6bf0783f8e055e1b86a6182e598f64274
Somebody32x2 <samuelshuster@gmail.com>
2024-01-10 23:13:36 -0500

    Finish Handshake & FULLY SEND (size limited) requests

--- http.js
+++ http.js
@@ -4,6 +4,13 @@ const crypto = require("crypto");
 const kyber = require('crystals-kyber');
 
 const server = net.createServer();
+const IV_U8_LABEL = new TextEncoder().encode("HTTQS-IV:");
+const PK_U8_LABEL = new TextEncoder().encode("HTTQS-PK:");
+const MESSAGE_U8_LABEL = new TextEncoder().encode("HTTQS-HANDSHAKE-MESSAGE:");
+
+const MTU = 14000;
+
+let knownUnsupportingServers = [];
 
 server.on('connection', (clientToProxySocket) => {
     // console.log('Client Connected To Proxy');
@@ -21,64 +28,114 @@ server.listen(8888, () => {
 server.on('connection', (clientToProxySocket) => {
     // console.log('Client Connected To Proxy');
     // We need only the data once, the starting packet
-    clientToProxySocket.once('data', (data) => {
-        try {
-            let isTLSConnection = data.toString().indexOf('CONNECT') !== -1;
-            // console.log(data.toString())
 
-            // Considering Port as 80 by default
-            let serverPort = 80;
-            let serverAddress;
-            if (isTLSConnection) {
-                // Port changed to 443, parsing the host from CONNECT
-                serverPort = 443;
-                serverAddress = data.toString()
-                    .split('CONNECT ')[1]
-                    .split(' ')[0];
-                // Parse and remove the port number
-                if (serverAddress.indexOf(':') !== -1) {
-                    serverPort = serverAddress.split(':')[1];
-                    serverAddress = serverAddress.split(':')[0];
-                }
-            } else {
-                // Parsing HOST from HTTP
-                serverAddress = data.toString()
-                    .split('Host: ')[1].split('\r\n')[0];
-                // Parse the port number if specified
-                if (serverAddress.indexOf(':') !== -1) {
-                    serverPort = serverAddress.split(':')[1];
-                    serverAddress = serverAddress.split(':')[0];
-                }
+
+    clientToProxySocket.once('data', (clientData) => {
+        let isTLSConnection = clientData.toString().indexOf('CONNECT') !== -1;
+        // console.log(data.toString())
+
+        // Considering Port as 80 by default
+        let serverPort = 80;
+        let serverAddress;
+        if (isTLSConnection) {
+            // Port changed to 443, parsing the host from CONNECT
+            serverPort = 443;
+            serverAddress = clientData.toString()
+                .split('CONNECT ')[1]
+                .split(' ')[0];
+            // Parse and remove the port number
+            if (serverAddress.indexOf(':') !== -1) {
+                serverPort = serverAddress.split(':')[1];
+                serverAddress = serverAddress.split(':')[0];
             }
-            console.log(`Connecting to ${serverAddress}:${serverPort}`)
+        } else {
+            // Parsing HOST from HTTP
+            serverAddress = clientData.toString()
+                .split('Host: ')[1].split('\r\n')[0];
+            // Parse the port number if specified
+            if (serverAddress.indexOf(':') !== -1) {
+                serverPort = serverAddress.split(':')[1];
+                serverAddress = serverAddress.split(':')[0];
+            }
+        }
+        console.log(`Connecting to ${serverAddress}:${serverPort}`)
 
-            clientToProxySocket.client = {state: "UNKNOWN"}
-            let proxyToHttqsSocket = net.createConnection({
-                host: serverAddress, port: 8889
-            });
-            proxyToHttqsSocket.on('error', (data) => {
-                // console.log(data.toString())
-                console.log(`${serverAddress}:${serverPort} does not support HTTQS (${data.toString()})`);
-            });
-            proxyToHttqsSocket.on("connect", () => {
-                proxyToHttqsSocket.write("HTTQS-HELLO")
-            });
-            proxyToHttqsSocket.on("data", (data) => {
-                if (data.toString().includes("HTTQS-PK:")) {
-                    clientToProxySocket.client.publicKey = JSON.parse(data.toString().split("HTTQS-PK:")[1])
-                    clientToProxySocket.client.symmetricKey = kyber.Encrypt768(clientToProxySocket.client.publicKey, kyber.GenerateKeyPair())
-                    clientToProxySocket.client.state = "SUPPORTED"
-                    clientToProxySocket.write("HTTQS-HANDSHAKE-MESSAGE:" + kyber.Encrypt768(clientToProxySocket.client.publicKey, clientToProxySocket.client.symmetricKey))
 
-                    clientToProxySocket.client.initalize()
-                } else if (data.toString().includes("HTTQS-IV:")) {
-                    clientToProxySocket.client.iv = data.toString().split("HTTQS-IV:")[1].split(",").map((x) => parseInt(x))
-                    clientToProxySocket.client.key = crypto.subtle.importKey("raw", clientToProxySocket.client.shared_secret, "AES-GCM", true, ["encrypt", "decrypt"])
-                    clientToProxySocket.client.lastUsed = Date.now()
-                    clientToProxySocket.write("HTTQS-IV:" + Object.values(clientToProxySocket.client.iv).join(",") + "\r\n"); // TODO: Use hex, b64, or raw instead of JSON string
+
+        let proxyToHttqsSocket = net.createConnection({
+            host: serverAddress, port: 8889
+        });
+        proxyToHttqsSocket.client = {state: "UNKNOWN"}
+        proxyToHttqsSocket.on('error', (data) => {
+            // console.log(data.toString())
+            console.log(`${serverAddress}:${serverPort} does not support HTTQS (${data.toString()})`);
+            httqsUnsupported();
+        });
+        proxyToHttqsSocket.on("connect", () => {
+            proxyToHttqsSocket.write("HTTQS-HELLO")
+        });
+        proxyToHttqsSocket.on("data", async (data) => {
+            console.log("GOT DATA FROM PROXY (likely encrypted)" + data.toString().slice(0, 10) + "... (" + data.toString().length + " bytes)\n");
+            if (data.slice(0, PK_U8_LABEL.byteLength).equals(PK_U8_LABEL)) {
+                let pk = new Uint8Array(data.slice(PK_U8_LABEL.byteLength));
+                let [c, ss] = kyber.Encrypt768(pk);
+                proxyToHttqsSocket.client.shared_secret = ss;
+
+                // console.dir({shared_secret: proxyToHttqsSocket.client.shared_secret})
+
+                proxyToHttqsSocket.client.state = "SUPPORTED"
+
+                // console.dir({handshake_message: JSON.stringify(c)})
+                let u8Message = new Uint8Array(c.length + MESSAGE_U8_LABEL.byteLength);
+                u8Message.set(MESSAGE_U8_LABEL, 0);
+                u8Message.set(c, MESSAGE_U8_LABEL.byteLength);
+                proxyToHttqsSocket.write(u8Message);
+
+            } else if (data.toString().includes("HTTQS-IV:")) {
+                let iv = new Uint8Array(16);
+                data.copy(iv, 0, IV_U8_LABEL.byteLength);
+                proxyToHttqsSocket.client.iv = iv;
+                proxyToHttqsSocket.client.key = await crypto.subtle.importKey("raw", proxyToHttqsSocket.client.shared_secret, "AES-GCM", true, ["encrypt", "decrypt"]);
+                proxyToHttqsSocket.client.state = "READY";
+                proxyToHttqsSocket.client.lastUsed = Date.now();
+
+                console.log(`${serverAddress}:${serverPort} COMPLETED HTTQS HANDSHAKE`);
+                // clientToProxySocket.dataHandler = HTTQS_DATA_HANDLER;
+                async function handleClientHTTQSData(data) {
+                    console.log("GOT HTTQS CLIENT DATA; ENCRYPTING BYTES: " + data.toString());
+                    let encrypted = await crypto.subtle.encrypt({
+                        name: "AES-GCM", iv: proxyToHttqsSocket.client.iv
+                    }, proxyToHttqsSocket.client.key, data);
+                    proxyToHttqsSocket.write(new Uint8Array(encrypted));
+                    // console.dir({iv: proxyToHttqsSocket.client.iv, key: await crypto.subtle.exportKey("jwk",proxyToHttqsSocket.client.key), data: encrypted})
                 }
-            });
 
+                if (!isTLSConnection) {
+                    handleClientHTTQSData(clientData)
+                }
+                clientToProxySocket.on("data", handleClientHTTQSData);
+
+            } else if (proxyToHttqsSocket.client.state === "READY") {
+                console.dir({
+                    iv: proxyToHttqsSocket.client.iv,
+                    key: await crypto.subtle.exportKey("jwk", proxyToHttqsSocket.client.key),
+                    data: data
+                })
+                console.log("GOT HTTQS SERVER DATA; DECRYPTING BYTES");
+                let decrypted = await crypto.subtle.decrypt({
+                    name: "AES-GCM", iv: proxyToHttqsSocket.client.iv
+                }, proxyToHttqsSocket.client.key, data);
+                console.log("DECRYPTED BYTES: " + decrypted.toString());
+                clientToProxySocket.write(new Uint8Array(decrypted));
+            }
+        });
+
+        clientToProxySocket.on("close", () => {
+            proxyToHttqsSocket.destroy();
+            console.log("Closing HTTQS connection to " + serverAddress + ":" + serverPort);
+        });
+
+        function httqsUnsupported() {
 
             let proxyToServerSocket = net.createConnection({
                 host: serverAddress, port: serverPort
@@ -89,24 +146,20 @@ server.on('connection', (clientToProxySocket) => {
                     //Send Back OK to HTTPS CONNECT Request
                     clientToProxySocket.write('HTTP/1.1 200 OK\r\n\n');
                 } else {
-                    proxyToServerSocket.write(data);
+                    proxyToServerSocket.write(clientData);
                 }
                 // Piping the sockets
                 clientToProxySocket.pipe(proxyToServerSocket);
                 proxyToServerSocket.pipe(clientToProxySocket);
 
                 proxyToServerSocket.on('error', (err) => {
-                    console.log('PROXY TO SERVER ERROR');
-                    console.log(err);
+                    console.log(`Error from server ${serverAddress}:${serverPort} : ${err}`);
                 });
             });
-            clientToProxySocket.on('error', err => {
-                console.log('CLIENT TO PROXY ERROR');
-                console.log(err);
-            });
-        } catch (err) {
-            console.log(err)
-            console.log(data.toString())
         }
+
+        clientToProxySocket.on('error', err => {
+            console.log(`Error from local client: ${err}`);
+        });
     });
 });
--- reverse_http.js
+++ reverse_http.js
@@ -4,24 +4,25 @@ const crypto = require("crypto");
 const kyber = require('crystals-kyber');
 
 const server = net.createServer()
+const IV_U8_LABEL = new TextEncoder().encode("HTTQS-IV:");
+const PK_U8_LABEL = new TextEncoder().encode("HTTQS-PK:");
+const MESSAGE_U8_LABEL = new TextEncoder().encode("HTTQS-HANDSHAKE-MESSAGE:");
+
+const MTU = 14000;
 
 class Client {
     constructor() {
         this.state = "UNKNOWN"
     }
+
     initalize() {
-        while (true) {
-            this.id = crypto.randomUUID();
-            if (!clients[this.id]) break;
-        }
         this.state = "SUPPORTED"
         let keys = kyber.KeyGen768();
         this.publicKey = keys[0];
         this.symmetricKey = keys[1];
-        this.lastUsed = Date.now()
-        this.created = Date.now()
     }
 }
+
 server.on('connection', (clientToProxySocket) => {
     // console.log('Client Connected To Proxy');
 });
@@ -37,73 +38,119 @@ server.listen(8889, () => {
 });
 server.on('connection', (clientToProxySocket) => {
     clientToProxySocket.client = new Client();
-    // console.log('Client Connected To Proxy');
-    // We need only the data once, the starting packet
-    clientToProxySocket.on('data', (data) => {
-        console.log(data.toString())
+    let proxyToServerSocket = undefined;
+    clientToProxySocket.on('data', async (data) => {
+        console.log("RECIEVED DATA FROM CLIENT:" + data.toString().slice(0, 40) + "... (" + data.toString().length + " bytes)")
         if (data.toString().includes("HTTQS-HELLO")) {
+            // This is a client that supports HTTQS, get ready to set up encryption using Kyber768
             clientToProxySocket.client.initalize()
-            clientToProxySocket.write("HTTQS-PK:" + JSON.stringify(clientToProxySocket.client.publicKey) + "\r\n") // TODO: Use hex, b64, or raw instead of JSON string
-        } else if (clientToProxySocket.client.state === "SUPPORTED" && data.toString().includes("HTTQS-HANDSHAKE-MESSAGE:")) {
-            clientToProxySocket.client.shared_secret = kyber.Decrypt768(data.toString().split("HTTQS-HANDSHAKE-MESSAGE:")[1], clientToProxySocket.client.symmetricKey)
+
+            // Write the public key to the client in raw bytes
+            let u8Pk = new Uint8Array(clientToProxySocket.client.publicKey.length + PK_U8_LABEL.byteLength);
+            u8Pk.set(PK_U8_LABEL, 0);
+            u8Pk.set(clientToProxySocket.client.publicKey, PK_U8_LABEL.byteLength);
+            clientToProxySocket.write(u8Pk);
+
+        } else if (clientToProxySocket.client.state === "SUPPORTED" && data.slice(0, MESSAGE_U8_LABEL.byteLength).equals(MESSAGE_U8_LABEL)) {
+            // We have received the handshake message, which we can use to derive the shared secret
+            clientToProxySocket.client.shared_secret = kyber.Decrypt768(Array.from(new Uint8Array(data.slice(MESSAGE_U8_LABEL.byteLength))), clientToProxySocket.client.symmetricKey)
             clientToProxySocket.client.state = "READY"
             clientToProxySocket.client.iv = crypto.getRandomValues(new Uint8Array(16));
-            clientToProxySocket.client.key = crypto.subtle.importKey("raw", clientToProxySocket.client.shared_secret, "AES-GCM", true, ["encrypt", "decrypt"])
-            clientToProxySocket.client.lastUsed = Date.now()
-            clientToProxySocket.write("HTTQS-IV:" + Object.values(clientToProxySocket.client.iv).join(",") + "\r\n"); // TODO: Use hex, b64, or raw instead of JSON string
+            // console.dir({shared_secret: clientToProxySocket.client.shared_secret})
+            // Use the shared secret to derive the bulk encryption (AES) key
+            clientToProxySocket.client.key = await crypto.subtle.importKey("raw", clientToProxySocket.client.shared_secret, "AES-GCM", true, ["encrypt", "decrypt"])
+
+            // Write the IV to the client in raw bytes
+            let u8Message = new Uint8Array(clientToProxySocket.client.iv.byteLength + IV_U8_LABEL.byteLength);
+            u8Message.set(IV_U8_LABEL, 0);
+            u8Message.set(clientToProxySocket.client.iv, IV_U8_LABEL.byteLength);
+            clientToProxySocket.write(u8Message);
+
+        } else if (clientToProxySocket.client.state === "READY") {
+            // console.dir({iv: clientToProxySocket.client.iv, key: await crypto.subtle.exportKey("jwk", clientToProxySocket.client.key), data: data})
+            let decrypted = await crypto.subtle.decrypt({
+                name: "AES-GCM", iv: clientToProxySocket.client.iv
+            }, clientToProxySocket.client.key, data);
+            console.log("DECRYPTED BYTES: " + new TextDecoder().decode(decrypted));
+
+            if (!proxyToServerSocket) {
+                // First time; connect to the server using the decrypted data (this is likely this same server but with a different port)
+                let clientData = Buffer.from(decrypted)
+                let clientReqString = clientData.toString();
+                let isTLSConnection = clientReqString.indexOf('CONNECT') !== -1;
+
+                // Considering Port as 80 by default
+                let serverPort = 80;
+                let serverAddress;
+                if (isTLSConnection) {
+                    // Port changed to 443, parsing the host from CONNECT
+                    serverPort = 443;
+                    serverAddress = clientReqString
+                        .split('CONNECT ')[1]
+                        .split(' ')[0];
+                    // Parse and remove the port number
+                    if (serverAddress.indexOf(':') !== -1) {
+                        serverPort = serverAddress.split(':')[1];
+                        serverAddress = serverAddress.split(':')[0];
+                    }
+                } else {
+                    // Parsing HOST from HTTP
+                    serverAddress = clientReqString
+                        .split('Host: ')[1].split('\r\n')[0];
+                    // Parse the port number if specified
+                    if (serverAddress.indexOf(':') !== -1) {
+                        serverPort = serverAddress.split(':')[1];
+                        serverAddress = serverAddress.split(':')[0];
+                    }
+                }
+                console.log(`Connecting to ${serverAddress}:${serverPort}`)
+                proxyToServerSocket = net.createConnection({
+                    host: serverAddress, port: serverPort
+                }, async () => {
+                    if (isTLSConnection) {
+                        // Send Back OK to HTTPS CONNECT Request
+                        let res = "HTTP/1.1 200 OK\r\n\n";
+                        let encrypted = await crypto.subtle.encrypt({
+                            name: "AES-GCM", iv: clientToProxySocket.client.iv
+                        }, clientToProxySocket.client.key, Buffer.from(res));
+                        clientToProxySocket.write(new Uint8Array(encrypted));
+                        console.log("SENT OK TO CLIENT");
+                        console.dir({
+                            iv: clientToProxySocket.client.iv,
+                            key: await crypto.subtle.exportKey("jwk", clientToProxySocket.client.key),
+                            data: new Uint8Array(encrypted)
+                        });
+                    } else {
+                        proxyToServerSocket.write(clientData);
+                    }
+
+                    proxyToServerSocket.on('error', (err) => {
+                        console.log(`Error from server ${serverAddress}:${serverPort} : ${err}`);
+                    });
+
+                    // On receiving data from the server send it to the client encrypted
+                    proxyToServerSocket.on('data', async (data) => {
+                        console.log("GOT SERVER DATA; ENCRYPTING BYTES: " + data.toString().slice(0, 40) + "... (" + data.toString().length + " bytes)\n");
+                        let encrypted = await crypto.subtle.encrypt({
+                            name: "AES-GCM", iv: clientToProxySocket.client.iv
+                        }, clientToProxySocket.client.key, data);
+                        console.dir({
+                            iv: clientToProxySocket.client.iv,
+                            key: await crypto.subtle.exportKey("jwk", clientToProxySocket.client.key),
+                            data: new Uint8Array(encrypted)
+                        })
+                        clientToProxySocket.write(new Uint8Array(encrypted));
+                    });
+                });
+            } else {
+                // Already connected to the server, just write the decrypted data
+                proxyToServerSocket.write(decrypted);
+            }
+            // clientToProxySocket.write(new Uint8Array(decrypted));
         }
-            // let isTLSConnection = data.toString().indexOf('CONNECT') !== -1;
-            // // console.log(data.toString())
-            //
-            // // Considering Port as 80 by default
-            // let serverPort = 80;
-            // let serverAddress;
-            // if (isTLSConnection) {
-            //     // Port changed to 443, parsing the host from CONNECT
-            //     serverPort = 443;
-            //     serverAddress = data.toString()
-            //         .split('CONNECT ')[1]
-            //         .split(' ')[0];
-            //     // Parse and remove the port number
-            //     if (serverAddress.indexOf(':') !== -1) {
-            //         serverPort = serverAddress.split(':')[1];
-            //         serverAddress = serverAddress.split(':')[0];
-            //     }
-            // } else {
-            //     // Parsing HOST from HTTP
-            //     serverAddress = data.toString()
-            //         .split('Host: ')[1].split('\r\n')[0];
-            //     // Parse the port number if specified
-            //     if (serverAddress.indexOf(':') !== -1) {
-            //         serverPort = serverAddress.split(':')[1];
-            //         serverAddress = serverAddress.split(':')[0];
-            //     }
-            // }
-            // console.log(`Connecting to ${serverAddress}:${serverPort}`)
-            // let proxyToServerSocket = net.createConnection({
-            //     host: serverAddress,
-            //     port: serverPort
-            // }, () => {
-            //     // console.log('PROXY TO SERVER SET UP');
-            //     // console.log(serverAddress)
-            //     if (isTLSConnection) {
-            //         //Send Back OK to HTTPS CONNECT Request
-            //         clientToProxySocket.write('HTTP/1.1 200 OK\r\n\n');
-            //     } else {
-            //         proxyToServerSocket.write(data);
-            //     }
-            //     // Piping the sockets
-            //     clientToProxySocket.pipe(proxyToServerSocket);
-            //     proxyToServerSocket.pipe(clientToProxySocket);
-            //
-            //     proxyToServerSocket.on('error', (err) => {
-            //         console.log('PROXY TO SERVER ERROR');
-            //         console.log(err);
-            //     });
-            // });
-            // clientToProxySocket.on('error', err => {
-            //     console.log('CLIENT TO PROXY ERROR');
-            //     console.log(err);
-            // });
+    });
+    clientToProxySocket.on('error', (err) => {
+        console.log('CLIENT ERROR');
+        console.log(err);
     });
 });
